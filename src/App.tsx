67996879aa15ce1472e6c6557a9f5f3e935765e1
heycn <heycn@foxmail.com>
2023-03-10 17:09:40 +0800

    🦄 refactor: 重构样式

--- src/App.tsx
+++ src/App.tsx
@@ -4,28 +4,37 @@ import avatar from './assets/avatar.jpg'
 import { Props } from './components/Link'
 import { Card } from './components/Card'
 import { Label } from './components/Label'
+import { BasicsType } from './components/Layout'
+import {Link} from './components/Link'
 
-const basics: string[] = ['男', '23岁', '前端开发', '退役军人']
-const contacts: string[] = ['电话: 123456789', '微信: heycn1', '邮箱: heycn@foxmail.com']
 const socialLinks: Props[] = [
   { href: 'https://juejin.cn/user/4372092371864984/posts', title: '掘金', label: '我目前已有 70 篇技术博客，被阅读量 1.5w+', direction: 'right' },
   { href: 'https://github.com/heycn', title: 'GitHub', label: '我的 GitHub 过去一年有 2000 次 commit', direction: 'right' },
   { href: 'https://heycn.github.io/', title: 'heycn.github.io', label: '我的个人博客网站', direction: 'right' }
 ]
+const contacts = {
+  wechat: 'heycn',
+  phone: '18888888888',
+  email: 'heycn@foxmail.com'
+}
+const basics: BasicsType = {
+  jobTitle: '前端开发工程师',
+  birthYear: 2000,
+  gender: 'male'
+}
 
 export const App: React.FC = () => {
   return (
     <Layout
       fullName='陈楠'
+      jobTitle='前端开发工程师'
+      contacts={contacts}
       avatar={avatar}
       basics={basics}
-      contacts={contacts}
-      socialLinks={socialLinks}
     >
       <Card title='关于我'>
         <Label>
-          哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
-          哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈哈
+
         </Label>
       </Card>
     </Layout>
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,43 +1,78 @@
 import * as React from 'react'
 import { Link } from './Link'
 import { Props as LinkType } from './Link'
+import icon_wechat from '../assets/wechat.svg'
+import icon_phone from '../assets/phone.svg'
+import icon_email from '../assets/email.svg'
+
+export type BasicsType = {
+  jobTitle: string
+  birthYear: number
+  gender: 'male' | 'female' | 'trans-female' | 'trans-male' | 'LGBTQ+'
+}
 
 type Props = {
   children?: React.ReactNode
   avatar: string
   fullName: string
-  basics: string[]
-  contacts: string[]
-  socialLinks?: LinkType[]
   linkStrong?: boolean
+  jobTitle: string
+  contacts: {
+    wechat: string
+    phone: number | string
+    email: string
+  }
+  basics: BasicsType
+}
+
+const genderMap = {
+  'male': '男',
+  'female': '女',
+  'trans-female': '女变性人',
+  'trans-male': '男变性人',
+  'LGBTQ+': 'LGBTQ+'
 }
 
 const Avatar: React.FC<{ src: string }> = ({ src }) => (
-  <div flex justify-center items-center w-35mm h-45mm overflow-hidden>
+  <div flex justify-center items-center w-35mm h-35mm overflow-hidden>
     <img src={src} w-auto h-full object-cover />
   </div>
 )
 
 export const Layout: React.FC<Props> = ({
-  avatar, children, fullName, linkStrong,
-  basics, contacts, socialLinks
-}) => (
-  <article className='bg-white w-21cm min-h-29.7cm p-5'>
-    <header flex p-b-4 p-t-4>
-      <Avatar src={avatar} />
-      <section m-l-2em flex flex-col justify-between>
-        <h1 c-black fw-900 text-size-2em>{fullName}</h1>
-        <p>{basics?.join(' | ')}</p>
-        <p>{contacts?.join(' | ')}</p>
-        {socialLinks?.map(s => (
-          <p>
-            <Link title={s.title} href={s.href} direction={s?.direction} label={s?.label} strong={linkStrong} />
+  avatar, children, fullName, basics: b, contacts: c
+}) => {
+
+  const contactList = [
+    { icon: icon_wechat, value: c.wechat },
+    { icon: icon_phone, value: c.phone },
+    { icon: icon_email, value: c.email }
+  ]
+
+  const basicsList = [
+    b.jobTitle, `${new Date().getFullYear() - b.birthYear}岁`, genderMap[b.gender]
+  ]
+
+  return (
+    <article className='bg-white w-21cm min-h-29.7cm p-5'>
+      <header flex items-center p-b-4 p-t-4>
+        <Avatar src={avatar} />
+        <section m-l-2em flex flex-col justify-between>
+          <h1 c-black fw-900 text-size-2em p-t-2 p-b-2>{fullName}</h1>
+          <p>{basicsList.join(' | ')}</p>
+          <p flex items-center>
+            {contactList.map(c => (
+              <span key={c.icon} flex items-center m-r-1em>
+                <img src={c.icon} w-4 m-r-1 />
+                {c.value}
+              </span>
+            ))}
           </p>
-        ))}
-      </section>
-    </header>
-    <main>
-      {children}
-    </main>
-  </article>
-)
+        </section>
+      </header>
+      <main>
+        {children}
+      </main>
+    </article>
+  )
+}
